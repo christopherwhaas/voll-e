faf016c3ba31b21a382b4975fe18bc6c55d75d55
christopherwhaas <cwhaas98@gmail.com>
2025-09-06 17:54:58 -0700

    update version

--- src/screens/TournamentScreen.tsx
+++ src/screens/TournamentScreen.tsx
@@ -1,5 +1,5 @@
 // // TEMPORARILY DISABLED - Tournament functionality
-// // This file has been commented out to remove tournament logic from the app
+// // This file has been commented out to remove tournament logic from the app 
 
 // /*
 // import * as React from 'react';
